6c5b24849577036f1d2d30cf4972ddc0c216a440
Carl Ehrett <cehrett@gmail.com>
2025-03-19 14:42:04 -0400

    Minor update to env installation

--- pytorch_advanced/create_env.sh
+++ pytorch_advanced/create_env.sh
@@ -1,8 +1,14 @@
-#!/usr/bin/bash
+#!/bin/bash
+#SBATCH --job-name=create_LLMs_env
+#SBATCH --mem=12G
+#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --output=create_LLMs_env_output.log
 
-ENV_NAME="PytorchWorkshop"
+# Palmetto modules
+module load miniforge3
 
-module load anaconda3
+ENV_NAME="PytorchWorkshop2"
 
 conda create -y -n $ENV_NAME python=3.11
 
